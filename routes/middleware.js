ec542e955103fc6f46f9d3a013c7cd23e194ba87
任天赋 <rentianfu@xiaomi.com>
2020-04-16 22:00:16 +0800

    fix:授权头像昵称无法更新的bug

--- routes/middleware.js
+++ routes/middleware.js
@@ -1,30 +1,24 @@
-const request = require('request-promise');
-const { validateUrl_wx } = require('./config');
-const getCache = require('./cache');
+const request = require("request-promise");
+const { validateUrl_wx } = require("./config");
 
-const sessionUser = async function(ctx, next) {
-  const ticket = ctx.request.header['x-ticket'];
+const sessionUser = async function (ctx, next) {
+  const ticket = ctx.request.header["x-ticket"];
   try {
-    const cache = getCache();
-    let user = cache.get(ticket);
-    if (!user) {
-      user = await request({
-        url: validateUrl_wx,
-        method: 'POST',
-        json: true,
-        body: {
-          ticket,
-        },
-      });
-      cache.set(ticket, user);
-    }
+    user = await request({
+      url: validateUrl_wx,
+      method: "POST",
+      json: true,
+      body: {
+        ticket,
+      },
+    });
     ctx.state.user = user;
     await next();
   } catch (error) {
     console.error(error);
     ctx.body = {
       code: 408,
-      error: '登录超时',
+      error: "登录超时",
     };
   }
 };
