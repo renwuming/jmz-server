afcee2b27e45d8f45ce654f1afb08c1569e9c1eb
任天赋 <rentianfu@xiaomi.com>
2020-07-28 10:41:08 +0800

    feat: 兼容pc微信登录

--- routes/middleware.js
+++ routes/middleware.js
@@ -2,7 +2,7 @@ const request = require("request-promise");
 const { validateUrl_wx, adminList } = require("./config");
 
 const sessionUser = async function (ctx, next) {
-  const ticket = ctx.request.header["x-ticket"];
+  const ticket = ctx.request.header["x-ticket"] || ctx.cookies.get("ticket");
   try {
     user = await request({
       url: validateUrl_wx,
